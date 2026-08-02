694bb7c4246977e09bb307ac5925a77a8b3f4227
ShahradLF <sharadfer@gmail.com>
2026-08-02 21:21:44 +0330

    feat(user): added User and country class in models.py feat(database) : database settings in setting.py

---
+++ User/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class City(models.Model):
+    city_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+
+class User(models.Model):
+
+    GENDER_CHOICES = (
+        ("male", "male"),
+        ("female", "female"),
+    )
+
+    NOISE_TOLERANCE_CHOICES = (
+        ("low", "low"),
+        ("medium", "medium"),
+        ("high", "high"),
+    )
+
+    TIME_FREQUENCY_CHOICES = (
+        ("never", "never"),
+        ("sometimes", "sometimes"),
+        ("often", "often"),
+        ("usually", "usually"),
+        ("always", "always")
+    )
+
+    PREFER_CHOICES = (
+        ("always together", "always together"),
+        ("sometimes hang out", "sometimes hang out"),
+        ("personal space", "personal space")
+    )
+
+    user_id = models.AutoField(primary_key=True)
+
+
+    # base infos
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='user', 
+        verbose_name="city"
+    )
+
+    job = models.CharField(max_length=20)
+    bio = models.CharField(max_length=200, blank=True, null=True)
+
+
+    # sleep
+    wake_up = models.TimeField()
+    sleep = models.TimeField()
+
+
+    # cleaning
+    cleanliness = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+
+    cleaning_frequency = models.CharField(max_length=30, choices=TIME_FREQUENCY_CHOICES)
+
+
+    # voice
+    noise_tolerance = models.CharField(max_length=30, choices=NOISE_TOLERANCE_CHOICES)
+
+
+    # smoking
+    smoking = models.BooleanField()
+
+
+    # pets
+    has_pets = models.BooleanField()
+    accept_pets = models.BooleanField()
+
+
+    # food
+    cooking = models.CharField(max_length=30, choices=TIME_FREQUENCY_CHOICES)
+    eating_at_home = models.CharField(max_lenght = 30, choices=TIME_FREQUENCY_CHOICES)
+
+
+    # guests
+    guests = models.CharField(TIME_FREQUENCY_CHOICES)
+    overnight_guests = models.CharField(max_length=40, choices=TIME_FREQUENCY_CHOICES)
+
+
+    # social style
+    social_level = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+
+    prefer = models.CharField(max_length=30, choices=PREFER_CHOICES)
